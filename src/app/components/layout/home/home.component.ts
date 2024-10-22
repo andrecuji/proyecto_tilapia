a9d1ef680bb622110b8ab369aabfaea34cdca087
andrecuji <candrescujia@uniguajira.edu.co>
2024-10-21 23:12:46 -0500

    comit

--- src/app/components/layout/home/home.component.ts
+++ src/app/components/layout/home/home.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from "../header/header.component";
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
